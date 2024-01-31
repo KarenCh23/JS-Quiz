6f85b4f4ae82002415011f520cbf5645854ff2c0
KarenCh23 <karenchamand@hotmail.fr>
2024-01-31 10:46:52 +0100

    display js questions on start button click

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ import { Questions } from "./questions";
 console.log(Questions);
 
 const app = document.querySelector("#app");
+
+const startButton = document.querySelector("#start");
+
+let i = 0;
+
+startButton.addEventListener("click", () => {
+ const question = document.querySelector("#question") ?? document.createElement("p")
+ question.id = "question";
+ question.innerText = Questions[i].question;
+ app.insertBefore(question, startButton);
+
+ i++;
+ if(i > Questions.length - 1) {
+  i = 0;
+ }
+});
